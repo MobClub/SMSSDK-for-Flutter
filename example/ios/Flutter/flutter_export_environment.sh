7f96a8f579ab5ae8bf95e1b39a1aba259f5e7dcb
攻玉网络技术部 <cyjaysong@cqgynet.com>
2020-03-24 17:02:35 +0800

    尝试修改注册方式

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/Shared/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gynet_2/Desktop/SMSSDK-for-Flutter/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Shared/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
