3fcbf991cf7f76fe3545eef4ce5f6dd952e4fed1
攻玉网络技术部 <cyjaysong@cqgynet.com>
2020-03-27 20:22:06 +0800

    尝试修改注册方式

--- example/android/app/src/main/java/cn/smssdk/demo/MainActivity.java
+++ example/android/app/src/main/java/cn/smssdk/demo/MainActivity.java
@@ -2,20 +2,13 @@ package cn.smssdk.demo;
 
 import androidx.annotation.NonNull;
 
-import cn.smssdk.flutter.MobsmsPlugin;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-	@Override
-	public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-		GeneratedPluginRegistrant.registerWith(flutterEngine);
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		MobsmsPlugin.recycle();
-	}
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
